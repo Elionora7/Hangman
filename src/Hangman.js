aaaf87bc6f5ca2e8fd2e3f866fd49c8794f9372a
Elionora <78257077+Elionora7@users.noreply.github.com>
2022-07-29 23:15:44 +1000

    add iswinner

--- src/Hangman.js
+++ src/Hangman.js
@@ -79,6 +79,8 @@ class Hangman extends Component {
       const isWinner = this.guessedWord().join("") === this.state.answer;
       const altText = `${this.state.nWrong}/${this.props.maxWrong} guesses`;
       let gameState = this.generateButtons();
+      if (isWinner) gameState = "You Win!";
+      if (gameOver) gameState = "You Lose!";
       return (
         <div className='Hangman'>
           <h1>Hangman</h1>
